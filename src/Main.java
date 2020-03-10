a6638514edad57fb2c62a17f53c5b99414a6721f
Aroli <1003994379@qq.com>
2020-03-10 14:15:50 +0800

    一个HelloWorld的java文件 Gai

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev");
-        System.out.println(123);
-        System.out.println(1234);
-        System.out.println(333);
-        System.out.println(444);
     }
 }
