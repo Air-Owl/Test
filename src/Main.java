3a730b46e530b198f36d3e8985c66ece807418da
Aroli <48898924+Air-Owl@users.noreply.github.com>
2020-03-10 14:30:39 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev");
-        System.out.println("origin dev");
+        System.out.println("remote dev");
         //change
     }
 }
